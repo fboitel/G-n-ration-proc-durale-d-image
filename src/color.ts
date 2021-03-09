f01568143278c6c76b0d4471d1fb91f4d82e0229
SACCOCCIO Clement <csaccoccio@enseirb-matmeca.fr>
2021-03-09 15:35:55 +0100

    added rgba getters

--- src/color.ts
+++ src/color.ts
@@ -1,4 +1,4 @@
-import { cons, PointedPair } from './pointed-pair'
+import { car, cdr, cons, PointedPair } from './pointed-pair'
 
 export interface Color extends PointedPair<PointedPair<number, number>, PointedPair<number, number>> {}
 
@@ -9,3 +9,18 @@ function color(r: number, g: number, b: number, a: number = 255): Color {
 	)
 }
 
+function red(c: Color): number {
+	return car(car(c))
+}
+
+function blue(c: Color): number {
+	return car(cdr(c))
+}
+
+function green(c: Color): number {
+	return cdr(car(c))
+}
+
+function alpha(c: Color): number {
+	return cdr(cdr(c))
+}
