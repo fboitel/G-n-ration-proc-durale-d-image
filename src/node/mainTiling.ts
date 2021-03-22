ade2203c532a28de047d55a230a39b33e7fc32ac
BOITEL Faustin <faustin.boitel@bordeaux-inp.fr>
2021-03-22 17:55:56 +0100

    small update for tiling

--- src/node/mainTiling.ts
+++ src/node/mainTiling.ts
@@ -2,7 +2,7 @@ import { createCanvas, createImageData } from 'canvas';
 import { writeFileSync } from 'fs';
 import { toRaster } from '../common/image';
 import { generators } from '../common/generators/generator'
-import { pavageCarréGen, pavageHexaGen, pavageTriangleGen, pavageCarréAdouciGen } from '../common/generators/pavage';
+import { pavageCarréGen, pavageHexaGen, pavageTriangleGen, pavageCarréAdouciGen } from '../common/generators/tiling';
 import { color } from '../common/color'
 
 // TEST
@@ -13,7 +13,7 @@ const rep = 10;
 
 //const img = pavageHexaGen(width, height, size, color(122, 122, 255, 255), color(255, 0, 0, 255), color(0, 255, 0, 255));
 //const img = pavageCarréGen(width, height, size, color(122, 122, 255, 255), color(255, 0, 0, 255));
-const img = pavageCarréAdouciGen(width, height, rep, color(0, 0, 0, 255), color(0, 255, 122, 255), color(122, 0, 255, 255));
+const img = pavageCarréAdouciGen(width, height, rep, color(255, 122, 0, 255), color(0, 255, 122, 255), color(122, 0, 255, 255));
 
 // OUTPUT
 
