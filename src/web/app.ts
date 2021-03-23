ef6f75b233a81c46539d49a0d6db0e65f1f8da51
SACCOCCIO Clement <csaccoccio@enseirb-matmeca.fr>
2021-03-23 17:02:10 +0100

    grabbable blocks

--- src/web/app.ts
+++ src/web/app.ts
@@ -24,6 +24,58 @@ function createOptionGroup(name: string, options: string[]): HTMLOptGroupElement
     return group;
 }
 
-const select = document.getElementById('generator-and-filters') as HTMLSelectElement
-select.appendChild(createOptionGroup('Générateurs', generators))
-select.appendChild(createOptionGroup('Filtres', filters))
+const select = document.getElementById('generator-and-filters') as HTMLSelectElement;
+select.appendChild(createOptionGroup('Générateurs', generators));
+select.appendChild(createOptionGroup('Filtres', filters));
+
+const graph = document.getElementById('graph') as HTMLDivElement;
+
+const button = document.getElementById('add-btn');
+button.addEventListener('click', () => {
+    const name = select.options[select.selectedIndex].value;
+    const element = document.createElement('div');
+    element.className = 'block';
+    element.textContent = name;
+    makeDraggable(element)
+    graph.appendChild(element);
+});
+
+function makeDraggable(element: HTMLDivElement) {
+    interface Position {
+        x: number;
+        y: number;
+    }
+
+    function getPos(e: MouseEvent): Position {
+        return {
+            x: e.clientX,
+            y: e.clientY,
+        };
+    }
+
+    element.addEventListener('mousedown', (e: MouseEvent) => {
+        let pos = getPos(e);
+
+        function drag(e: MouseEvent) {
+            const newPos = getPos(e);
+
+            const offset = {
+                x: pos.x - newPos.x,
+                y: pos.y - newPos.y,
+            };
+
+            element.style.top = (element.offsetTop - offset.y) + 'px';
+            element.style.left = (element.offsetLeft - offset.x) + 'px';
+
+            pos = newPos;
+        }
+
+        function stopDrag() {
+            document.removeEventListener('mousemove', drag);
+            document.removeEventListener('mouseup', stopDrag);
+        }
+
+        document.addEventListener('mouseup', stopDrag);
+        document.addEventListener('mousemove', drag);
+    });
+}
