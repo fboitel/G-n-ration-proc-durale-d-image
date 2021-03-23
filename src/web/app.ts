65417cdb36539621c3af0fa28e40c735c518223d
SACCOCCIO Clement <csaccoccio@enseirb-matmeca.fr>
2021-03-23 17:03:51 +0100

    formatting

--- src/web/app.ts
+++ src/web/app.ts
@@ -1,27 +1,27 @@
 const generators = [
-    'gen 1',
-    'gen 2',
-    'gen 3',
+	'gen 1',
+	'gen 2',
+	'gen 3',
 ];
 
 const filters = [
-    'filter 1',
-    'filter 2',
-    'filter 3',
+	'filter 1',
+	'filter 2',
+	'filter 3',
 ];
 
 function createOptionGroup(name: string, options: string[]): HTMLOptGroupElement {
-    const group = document.createElement('optgroup');
-    group.label = name;
+	const group = document.createElement('optgroup');
+	group.label = name;
 
-    for (const optionName of options) {
-        const option = document.createElement('option');
-        option.value = optionName;
-        option.textContent = optionName;
-        group.appendChild(option);
-    }
+	for (const optionName of options) {
+		const option = document.createElement('option');
+		option.value = optionName;
+		option.textContent = optionName;
+		group.appendChild(option);
+	}
 
-    return group;
+	return group;
 }
 
 const select = document.getElementById('generator-and-filters') as HTMLSelectElement;
@@ -32,50 +32,50 @@ const graph = document.getElementById('graph') as HTMLDivElement;
 
 const button = document.getElementById('add-btn');
 button.addEventListener('click', () => {
-    const name = select.options[select.selectedIndex].value;
-    const element = document.createElement('div');
-    element.className = 'block';
-    element.textContent = name;
-    makeDraggable(element)
-    graph.appendChild(element);
+	const name = select.options[select.selectedIndex].value;
+	const element = document.createElement('div');
+	element.className = 'block';
+	element.textContent = name;
+	makeDraggable(element)
+	graph.appendChild(element);
 });
 
 function makeDraggable(element: HTMLDivElement) {
-    interface Position {
-        x: number;
-        y: number;
-    }
+	interface Position {
+		x: number;
+		y: number;
+	}
 
-    function getPos(e: MouseEvent): Position {
-        return {
-            x: e.clientX,
-            y: e.clientY,
-        };
-    }
+	function getPos(e: MouseEvent): Position {
+		return {
+			x: e.clientX,
+			y: e.clientY,
+		};
+	}
 
-    element.addEventListener('mousedown', (e: MouseEvent) => {
-        let pos = getPos(e);
+	element.addEventListener('mousedown', (e: MouseEvent) => {
+		let pos = getPos(e);
 
-        function drag(e: MouseEvent) {
-            const newPos = getPos(e);
+		function drag(e: MouseEvent) {
+			const newPos = getPos(e);
 
-            const offset = {
-                x: pos.x - newPos.x,
-                y: pos.y - newPos.y,
-            };
+			const offset = {
+				x: pos.x - newPos.x,
+				y: pos.y - newPos.y,
+			};
 
-            element.style.top = (element.offsetTop - offset.y) + 'px';
-            element.style.left = (element.offsetLeft - offset.x) + 'px';
+			element.style.top = (element.offsetTop - offset.y) + 'px';
+			element.style.left = (element.offsetLeft - offset.x) + 'px';
 
-            pos = newPos;
-        }
+			pos = newPos;
+		}
 
-        function stopDrag() {
-            document.removeEventListener('mousemove', drag);
-            document.removeEventListener('mouseup', stopDrag);
-        }
+		function stopDrag() {
+			document.removeEventListener('mousemove', drag);
+			document.removeEventListener('mouseup', stopDrag);
+		}
 
-        document.addEventListener('mouseup', stopDrag);
-        document.addEventListener('mousemove', drag);
-    });
+		document.addEventListener('mouseup', stopDrag);
+		document.addEventListener('mousemove', drag);
+	});
 }
