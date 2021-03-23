ca4a726727949362af04b490886771c8738891fb
BOITEL Faustin <faustin.boitel@bordeaux-inp.fr>
2021-03-23 15:21:09 +0100

    ééééééé

--- src/node/mainTiling.ts
+++ src/node/mainTiling.ts
@@ -2,18 +2,18 @@ import { createCanvas, createImageData } from 'canvas';
 import { writeFileSync } from 'fs';
 import { toRaster } from '../common/image';
 import { generators } from '../common/generators/generator'
-import { pavageCarréGen, pavageHexaGen, pavageTriangleGen, pavageCarréAdouciGen } from '../common/generators/tiling';
+import { pavageCarreGen, pavageHexaGen, pavageTriangleGen, pavageCarréAdouciGen, pavageGrandRhombitrihexagonalGen } from '../common/generators/tiling';
 import { color } from '../common/color'
 
 // TEST
 
 const width = 1000;
 const height = 1000;
-const rep = 10;
+const rep = 7;
 
 //const img = pavageHexaGen(width, height, size, color(122, 122, 255, 255), color(255, 0, 0, 255), color(0, 255, 0, 255));
-//const img = pavageCarréGen(width, height, size, color(122, 122, 255, 255), color(255, 0, 0, 255));
-const img = pavageCarréAdouciGen(width, height, rep, color(255, 122, 0, 255), color(0, 255, 122, 255), color(122, 0, 255, 255));
+//const img = pavageCarreGen(width, height, size, color(122, 122, 255, 255), color(255, 0, 0, 255));
+const img = pavageGrandRhombitrihexagonalGen(width, height, rep, color(255, 0, 0, 255), color(0, 255, 0, 255), color(0, 0, 255, 255));
 
 // OUTPUT
 
