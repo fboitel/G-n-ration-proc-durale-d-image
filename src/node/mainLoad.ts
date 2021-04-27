22304e2d944bfec391a11a0d1fcaf737140a1e40
BOITEL Faustin <faustin.boitel@bordeaux-inp.fr>
2021-04-27 19:30:45 +0200

    conflict

--- src/node/mainLoad.ts
+++ src/node/mainLoad.ts
@@ -1,3 +1,4 @@
+/*
 import { BLACK, BLUE, WHITE } from '../common/color';
 import { loadFromFile, saveToPNG } from '../common/file';
 import { filtersNew as filters } from '../common/filters/filter';
@@ -12,14 +13,15 @@ const mandrill = loadFromFile('images/mandrill.jpg');
 const white = consImage(500, 500, (x, y) => WHITE);
 const black = consImage(500, 500, (x, y) => BLACK);
 
-/*
+
 hawkeye.then(img => saveToPNG(filters.gaussianBlur(img), 'gaussblur'));
 hawkeye.then(img => saveToPNG(filters.sharpen(img), 'sharpen'));
 hawkeye.then(img => saveToPNG(filters.edgeDetection(img), 'detection'));
-*/
+
 hawkeye.then(img => saveToPNG(filters.colorimetry.negative(img), 'negate'));
 hawkeye.then(img => saveToPNG(filters.composition.multiply(img, white), 'white'));
 hawkeye.then(img => saveToPNG(filters.composition.multiply(img, black), 'black'));
 Promise.all([red, green, blue]).then((imgs) => saveToPNG(filters.composition.minus(filters.composition.plus(filters.composition.plus(imgs[0], imgs[1]), imgs[2]), imgs[0]), 'add'));
 hawkeye.then(img => saveToPNG(filters.colorimetry.opacity(img, -.5), 'opacity'));
 mandrill.then(img => saveToPNG(filters.colorimetry.brightness(img, .5), 'brighten'));
+*/
