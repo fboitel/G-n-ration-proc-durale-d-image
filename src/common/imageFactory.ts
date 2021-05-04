a01fe2c2f2baf24ff6035207e1b85b5fe111031f
LANGLAIS Hugo <Hugo.Langlais@bordeaux-inp.fr>
2021-05-04 17:04:31 +0200

    change image creator from json

---
+++ src/common/imageFactory.ts
@@ -0,0 +1,171 @@
+import { createCanvas, createImageData } from 'canvas';
+import { readFileSync, writeFileSync, existsSync } from 'fs';
+import { toRaster, Image } from '../common/image';
+import { generators, filters } from '../common/registry';
+import { GREEN, RED } from '../common/color';
+import { exit } from 'node:process';
+
+
+export function generateJSON(): string {
+
+    // FIXME : what happened if there is no params ?
+
+    // generator
+    const generatorRed = `{"type": "generator",
+						  	  "name": "monochromaticRed",
+						  	  "params" : { "width": 1000, "height": 1000 }
+   							}`;
+
+    // generator
+    const generatorGreen = `{"type": "generator",
+						  	  "name": "monochromaticGreen",
+						  	  "params" : { "width": 1000, "height": 1000}
+   							}`;
+    // generator
+    const generatorBlue = `{"type": "generator",
+						  	  "name": "monochromaticBlue",
+						  	  "params" : { "width": 1000, "height": 1000}
+   							}`;
+
+    // generator
+    const generator = `{"type": "generator",
+						  "name": "voronoi",
+						  "params" : { "width": 1000, "height": 1000, "nbPoints" : 20 }
+   						}`;
+
+    // filter <-- generator
+    const filterGenerator = `{"type": "filter",
+				 			  "name": "blue",
+				 			  "input":` + generator + `,
+				  			  "params": {"size": 2 }
+				 			}`;
+
+    // filter <-- generator
+    const filterGenerator2 = `{"type": "filter",
+				 			  "name": "darken",
+				 			  "input":` + generatorRed + `,
+				  			  "params": {"size": 2 }
+				 			}`;
+
+    // filter <-- filter <-- generator
+    const filterFilterGenerator = `{"type": "filter",
+									  "name": "brighten",
+									  "input":` + filterGenerator + `,
+									  "params": {"size": 2 }
+									}`;
+
+    // merge <-- generator + generator
+    const mergeGeneratorAndGenerator = `{"type": "merge",
+									  "name": "composePlus",
+									  "firstInput":` + generatorRed + `,
+									  "secondInput" : ` + generatorGreen + `,
+									  "params": {"size": 2 }
+  									}`;
+
+    // merge <-- merge + generator <-- generator + generator
+    const mergeGeneratorAndMergeGeneratorAndGenerator = `{"type": "merge",
+									  "name": "composePlus",
+									  "firstInput":` + mergeGeneratorAndGenerator + `,
+									  "secondInput" : ` + generatorBlue + `,
+									  "params": {"size": 2 }
+  									}`;
+
+    // merge <-- merge + generator <-- generator + generator
+    const mergeGeneratorAndMergeGeneratorAndGenerator2 = `{"type": "merge",
+									  "name": "composePlus",
+									  "firstInput":` + filterGenerator2 + `,
+									  "secondInput" : ` + filterFilterGenerator + `,
+									  "params": {"size": 2 }
+  									}`;
+
+    return generator;
+}
+/*
+export function parseParams(params: any): Array<any> {
+    let array = [];
+
+    for (let p in params) {
+        switch (params[p]) {
+
+            // FIXME : Params are strings, how parse a color ?
+            case "RED":
+                array.push(RED);
+                break;
+
+            case "GREEN":
+                array.push(GREEN);
+                break;
+
+            default:
+                array.push(params[p]);
+        }
+    }
+    return array;
+}*/
+
+
+export function parseParams(isGenerator: boolean, params: any): Array<any> {
+    let array = [];
+
+    for (let p in params) {
+        switch (params[p]) {
+
+            // FIXME : Params are strings, how parse a color ?
+            case "RED":
+                array.push(RED);
+                break;
+
+            case "GREEN":
+                array.push(GREEN);
+                break;
+
+            default:
+                array.push(params[p]);
+        }
+    }
+    return array;
+}
+
+export function readJSON(json: any): Image {
+
+    let img: Image;
+
+    let type = json["type"];
+    let name = json["name"];
+    let params = json["params"];
+    let parsedParams = null;
+
+    // FIXME : check type of parameters.ts
+    switch (type) {
+
+        case "generator":
+            parsedParams = parseParams(true, params);
+            img = generators[name].generator.apply(this, parsedParams);
+            break;
+
+        case "filter":
+            parsedParams = parseParams(false, params);
+
+            let inputs = json["inputs"];
+
+            if (inputs.length == filters[name].additionalInputs + 1) {
+                for (let i = 0; i < inputs.length; ++i) {
+
+                    let inp = readJSON(inputs[i]);
+                    
+                    if( inp == null) {
+                        return null;
+                    }
+
+                    parsedParams.unshift(inp);
+                }
+            } else {
+                return null;
+            }
+
+            img = filters[name].filter.apply(this, parsedParams);
+
+            break;
+    }
+    return img;
+}
--- src/node/main.ts
+++ src/node/main.ts
@@ -2,8 +2,8 @@ import { createCanvas, createImageData } from 'canvas';
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 import { toRaster, Image } from '../common/image';
 import { generators, filters } from '../common/registry';
-import { GREEN, RED } from '../common/color';
 import { loadFromFile } from './file';
+import { generateJSON, readJSON } from '../common/imageFactory'
 
 
 function exportToPNG(img: Image, name: string): void {
@@ -14,145 +14,8 @@ function exportToPNG(img: Image, name: string): void {
 	writeFileSync('public/' + name + '.png', buffer);
 }
 
+function main(): void {
 
-function generateJSON(): string {
-
-	// FIXME : what happened if there is no params ?
-
-	// generator
-	const generatorRed = `{"type": "generator",
-						  	  "name": "monochromaticRed",
-						  	  "params" : { "width": 1000, "height": 1000 }
-   							}`;
-
-	// generator
-	const generatorGreen = `{"type": "generator",
-						  	  "name": "monochromaticGreen",
-						  	  "params" : { "width": 1000, "height": 1000}
-   							}`;
-	// generator
-	const generatorBlue = `{"type": "generator",
-						  	  "name": "monochromaticBlue",
-						  	  "params" : { "width": 1000, "height": 1000}
-   							}`;
-
-	// generator
-	const generator = `{"type": "generator",
-						  "name": "voronoi",
-						  "params" : { "width": 1000, "height": 1000, "nbPoints" : 20 }
-   						}`;
-
-	// filter <-- generator
-	const filterGenerator = `{"type": "filter",
-				 			  "name": "blue",
-				 			  "input":` + generator + `,
-				  			  "params": {"size": 2 }
-				 			}`;
-
-	// filter <-- generator
-	const filterGenerator2 = `{"type": "filter",
-				 			  "name": "darken",
-				 			  "input":` + generatorRed + `,
-				  			  "params": {"size": 2 }
-				 			}`;
-
-	// filter <-- filter <-- generator
-	const filterFilterGenerator = `{"type": "filter",
-									  "name": "brighten",
-									  "input":` + filterGenerator + `,
-									  "params": {"size": 2 }
-									}`;
-
-	// merge <-- generator + generator
-	const mergeGeneratorAndGenerator = `{"type": "merge",
-									  "name": "composePlus",
-									  "firstInput":` + generatorRed + `,
-									  "secondInput" : ` + generatorGreen + `,
-									  "params": {"size": 2 }
-  									}`;
-
-	// merge <-- merge + generator <-- generator + generator
-	const mergeGeneratorAndMergeGeneratorAndGenerator = `{"type": "merge",
-									  "name": "composePlus",
-									  "firstInput":` + mergeGeneratorAndGenerator + `,
-									  "secondInput" : ` + generatorBlue + `,
-									  "params": {"size": 2 }
-  									}`;
-
-	// merge <-- merge + generator <-- generator + generator
-	const mergeGeneratorAndMergeGeneratorAndGenerator2 = `{"type": "merge",
-									  "name": "composePlus",
-									  "firstInput":` + filterGenerator2 + `,
-									  "secondInput" : ` + filterFilterGenerator + `,
-									  "params": {"size": 2 }
-  									}`;
-
-	return generator;
-}
-
-function parseParams(params: any): Array<any> {
-	let array = [];
-
-	for (let p in params) {
-		switch (params[p]) {
-
-			// FIXME : Params are strings, how parse a color ?
-			case "RED":
-				array.push(RED);
-			break;
-			
-			case "GREEN":
-				array.push(GREEN);
-			break;
-			
-			default:
-				array.push(params[p]);
-		}
-	}
-	return array;
-}
-
-function readJSON(json: any): Image {
-
-	let img: Image;
-
-	let type = json["type"];
-	let name = json["name"];
-	let params = json["params"];
-
-	let parsedParams = parseParams(params);
-
-	// FIXME : check type of parameters.ts
-	switch (type) {
-
-		case "generator":
-			img = generators[name].generator.apply(this, parsedParams);
-			break;
-
-		case "filter":
-			let input = readJSON(json["input"]);
-			parsedParams.unshift(input);
-			img = filters[name].filter.apply(this, parsedParams);
-			break;
-
-		case "merge":
-			let firstInput = readJSON(json["firstInput"]);
-			let secondInput = readJSON(json["secondInput"]);
-			parsedParams.unshift(secondInput);
-			parsedParams.unshift(firstInput);
-			img = filters[name].filter.apply(this, parsedParams);
-			break;
-	}
-
-	// save all steps
-	exportToPNG(img, name);
-
-	return img;
-}
-
-function mainJSON(): void {
-
-	
 	let jsonBuffer = "";
 	let fileName = "generatedImage"
 
@@ -176,8 +39,10 @@ function mainJSON(): void {
 	// TODO : check the order of the args
 	let img = readJSON(json);
 
-loadFromFile("./public/fleur.png").then(img => exportToPNG(filters.bilinearResize.filter(img,500,500), 'newfleur'));
-//	exportToPNG(img, fileName);
+	//loadFromFile("./public/fleur.png").then(img => exportToPNG(filters.bilinearResize.filter(img,500,500), 'newfleur'));
+	if (img != null) {
+		exportToPNG(img, fileName);
+	}
 }
 
-mainJSON();
+main();
